28d3d94fa2be770fbb919dfdc37717d03466cd40
myname <madcowog1@protonmail.com>
2021-08-23 14:16:07 -0700

    Still dont know if this works

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
 mkdir ffmpeg
 cd ffmpeg
-Invoke-WebRequest -Uri 'https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2021-08-14-12-36/ffmpeg-n4.4-80-gbf87bdd3f6-win64-gpl-4.4.zip' -UseBasicParsing -Outfile= './ffmpeg.zip'
+Invoke-WebRequest -Uri 'https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2021-08-14-12-36/ffmpeg-n4.4-80-gbf87bdd3f6-win64-gpl-4.4.zip' -UseBasicParsing -Outfile './ffmpeg.zip'
 Expand-Archive ./'ffmpeg-n4.4-80-gbf87bdd3f6-win64-gpl-4.4.zip' 
 cd ffmpeg
 cd 'ffmpeg-n4.4-80-gbf87bdd3f6-win64-gpl-4.4'
