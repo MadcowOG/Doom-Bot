35499628b5a56ef1c16a0724c3d181ebb80441b3
myname <madcowog1@protonmail.com>
2021-08-23 16:12:59 -0700

    Stupid Mistake

---
+++ setup.ps1
@@ -0,0 +1,13 @@
+mkdir ffmpeg
+cd ffmpeg
+Invoke-WebRequest -Uri 'https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2021-08-14-12-36/ffmpeg-n4.4-80-gbf87bdd3f6-win64-gpl-4.4.zip' -UseBasicParsing -Outfile './ffmpeg.zip'
+Expand-Archive 'ffmpeg.zip' 
+cd ffmpeg
+cd 'ffmpeg-n4.4-80-gbf87bdd3f6-win64-gpl-4.4'
+cd bin
+$variable =  Get-Location
+$newvariable = (';'+$variable)
+# TODO! Get this to add to the system environment variable 'Path' while also including previous additions
+$Path = [Environment]::GetEnvironmentVariable("Path", "Machine")
+$NewPath = $Path + $newvariable
+[System.Environment]::SetEnvironmentVariable("Path", $NewPath, "Machine")
