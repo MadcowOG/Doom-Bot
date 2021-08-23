12736ae7a2341ec7d6884b84185541fcb9d705b1
myname <madcowog1@protonmail.com>
2021-08-23 16:58:29 -0700

    Added ffmpeg options - test

--- setup.py
+++ setup.py
@@ -13,16 +13,15 @@ def setup(token, token_name, whitelist_user=None, whitelist_server=None, whiteli
         whitelist["servers"] = [whitelist_server]
         with open('whitelist.json', 'w') as file:
             json.dump(whitelist, file, indent=5)
-        print('!--Will now run a powershell script to download ffmpeg and set environment path--!')
-        p = subprocess.Popen(['powershell.exe', 'setup.ps1'], stdout=sys.stdout)
-        p.communicate()
-        return print('Thank you')
+        return print('Token.json and whitelist.json configured')
     else:
-        print('!--Will now run a powershell script to download ffmpeg and set environment path--!')
-        p = subprocess.Popen(['powershell.exe', './setup.ps1'], stdout=sys.stdout)
-        p.communicate()
-        return print('Thank you')
+        return print('Token.json configured')
 
+def ffmpeg_setup():
+    print('!--Will now run a powershell script to download ffmpeg and set environment path--!')
+    p = subprocess.Popen(['powershell.exe', 'setup.ps1'], stdout=sys.stdout)
+    p.communicate()
+    return print('ffmpeg downloaded and configured')
 
 with open('bot_start', 'r') as file:
     DOOM = file.read()
@@ -38,17 +37,40 @@ print(final)
 token_name = input('What name would you like for your token? ')
 token = input('What is your token? ')
 bool_whitelist = input('Would you like to configure the command whitelist? ')
+bool_ffmpeg = input('Would you like to install and configure ffmpeg?**Recommended Unless already done** ')
 correct_val = False
 while correct_val is not True:
-    if bool_whitelist.lower() == 'yes':
+    if bool_whitelist.lower() == 'yes' or bool_whitelist.lower() == 'y':
         whitelist_user = input('What is the user id you would like to whitelist? ')
         whitelist_server = input('What is the server id you would like to whitelist? ')
+        correct_val_ffmpeg = False
+        while correct_val_ffmpeg is not True:
+            if bool_ffmpeg.lower() == 'yes' or bool_ffmpeg.lower() == 'y':
+                ffmpeg_setup()
+                correct_val_ffmpeg = True
+            elif bool_ffmpeg.lower() == 'no' or bool_ffmpeg.lower() == 'n':
+                correct_val_ffmpeg = True
+            else:
+                print('Not a valid input')
+                bool_ffmpeg = input('Would you like to install and configure ffmpeg?**Recommended Unless already done** ')
         setup(token=token, token_name=token_name, whitelist_user=whitelist_user, whitelist_server=whitelist_server, whitelist_bool=True)
         correct_val = True
+        print('Thank you')
         sys.exit()
     elif bool_whitelist.lower() == 'no':
         setup(token=token, token_name=token_name)
+        correct_val_ffmpeg = False
+        while correct_val_ffmpeg is not True:
+            if bool_ffmpeg.lower() == 'yes' or bool_ffmpeg.lower() == 'y':
+                ffmpeg_setup()
+                correct_val_ffmpeg = True
+            elif bool_ffmpeg.lower() == 'no' or bool_ffmpeg.lower() == 'n':
+                correct_val_ffmpeg = True
+            else:
+                print('Not a valid input')
+                bool_ffmpeg = input('Would you like to install and configure ffmpeg?**Recommended Unless already done** ')
         correct_val = True
+        print('Thank you')
         sys.exit()
     else:
         print('Not a valid input')
